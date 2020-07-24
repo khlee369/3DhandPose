0aa2cd494ee22bab756e4bd7d2ff5df15cbacd1e
Lee kwang ho <lex.less38@gmail.com>
2020-07-25 02:42:10 +0900

    load batch implemented
    it's so heavy to load all data at once so it will read files per each batch

--- loader.py
+++ loader.py
@@ -2,6 +2,7 @@ import pickle
 import os
 import numpy as np
 import cv2
+import random
 
 # 
 # set = 'training'
@@ -9,6 +10,7 @@ set = 'evaluation'
 f1 = open('./RHD_published_v2/{}/anno_{}.pickle'.format(set, set), 'rb')
 anno_all = pickle.load(f1)
 f1.close()
+max_len = len(anno_all)
 
 path_to_db = './RHD_published_v2/'
 
@@ -23,3 +25,33 @@ def load_id(sample_id):
     depth = cv2.cvtColor(depth, cv2.COLOR_BGR2RGB)
 
     return image, mask, depth
+
+# data is so heavy to load all, so read the file per each batch
+def load_batch(batch_size):
+    image_list = []
+    mask_list = []
+    depth_list = []
+    anno_list = []
+    for i in range(batch_size):
+        idx = random.randint(0, max_len-1)
+        image, mask, depth = load_id(idx)
+        anno = anno_all[idx]
+
+        image_list.append(image)
+        mask_list.append(mask)
+        depth_list.append(depth)
+        anno_list.append(anno)
+
+    return np.array(image_list), np.array(mask_list), np.array(depth_list), np.array(anno_list)
+
+def load_batch_croped(batch_size):
+    imgs, masks, depths, annos = load_batch(batch_size)
+    concat3 = tf.concat([imgs, masks, depths], axis=0)
+    concat3_croped = tf.random_crop(concat3, [3*batch_size], 256, 256, 3)
+    imgs_crop = concat3_croped[:batch_size]
+    masks_crop = concat3_croped[batch_size : 2*batch_size]
+    depths_crop = concat3_croped[2*batch_size : 3*batch_size]
+
+    return imgs_crop, masks_crop, depths_crop, annos
+
+    
